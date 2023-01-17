fcc1c00fb3b2e07db35239e77a845d919975932c
Emanuele-belfiori <121684864+Emanuele-belfiori@users.noreply.github.com>
2023-01-17 12:07:52 +0100

    Secondo commit

--- src/Main.java
+++ src/Main.java
@@ -8,32 +8,33 @@ public class Main {
         riga2     8     6    10
          */
 
-       //  I forma:
-        int[][] dati= new int[][]{
-                new int[] {1, 5, 7},
-                new int[] {8, 6, 10}
+        //  I forma:
+        int[][] dati = new int[][]{
+                new int[]{1, 5, 7},
+                new int[]{8, 6, 10}
         };
 
         // II forma:
-        int[][] arrayDati ={
+        int[][] arrayDati = {
                 {1, 5, 7},
                 {8, 6, 10}
         };
 
         // III forma:
-        int[][] mioArray = new int [][]{
-          //    Trimestre    I   II   III  IV
-                new int [] {120, 125, 180, 140}, // 2022 incassi totale riga
-                           {178, 180, 141, 135}, // 2021
-                           {149, 178, 134, 138}  // 2020
+        int[][] mioArray = new int[][]{
+                //    Trimestre    I   II   III  IV
+                new int[]{120, 125, 180, 140}, // 2022 incassi totale riga
+                {178, 180, 141, 135}, // 2021
+                {149, 178, 134, 138},// 2020
+                {178, 180, 141, 135}, // 2019
         };
 
         // Per itarare un array bidimensionale usiamo (normalmente):
         // creiamo un doppio for: uno esterno e uno interno:
         // esterno---> itera RIGHE
         // interno---> itera COLONNE
-        for (int i =0; i<mioArray.length; i++){
-            for (int j =0; j< mioArray[i].length; j++){
+        for (int i = 0; i < mioArray.length; i++) {
+            for (int j = 0; j < mioArray[i].length; j++) {
                 System.out.print(mioArray[i][j] + "\t");
             }
             System.out.println();
@@ -50,21 +51,19 @@ public class Main {
         }
 
         // Ciclo foreach con singolo array
-        int[] datiProva = new int[]{1, 5 , 7 };
-        for (int dato: datiProva) {
+        int[] datiProva = new int[]{1, 5, 7};
+        for (int dato : datiProva) {
             System.out.print(dato + " ");
 
         }
 
 
-        // usando l'array mioarray sommo i valori  dei singoli array
-
         System.out.println("\nCalcolo il valore degli incassi dei singoli annuali");
         int anno = 2020;
         int somma = 0;
 
-        for (int riga =0; riga<mioArray.length; riga++){
-            for (int colonna = 0; colonna<mioArray[riga].length; colonna++){
+        for (int riga = 0; riga < mioArray.length; riga++) {
+            for (int colonna = 0; colonna < mioArray[riga].length; colonna++) {
                 //calcola il totale di ogni singola riga
                 somma = somma + mioArray[riga][colonna];
             }
@@ -74,13 +73,60 @@ public class Main {
         }
 
         // Somma per trimestri (sommo i valori delle colonne)
-        System.out.println("Totale per trimestri");
-        for (int righe= 0; righe< mioArray[0].length; righe++){
-            for (int colonne= 0; colonne<mioArray[righe].length; colonne++){
+        /*
+         int[][] mioArray = new int [][]{
+          //    Trimestre    I   II   III  IV
+                new int []{120, 125, 180, 140}, // 2022 incassi totale riga
+                          {178, 180, 141, 135}, // 2021
+                          {149, 178, 134, 138} // 2020
+         */
+
+        System.out.println();
+
+        int sommacolonne = 0;
+        String totaliProgressivi = "";
+        for (int ciclo = 0; ciclo < 1; ciclo++) {
+            for (int colonne = 0; colonne < mioArray[0].length; colonne++) {
+
+
+                for (int righe = 0; righe < mioArray.length; righe++) {
+                    System.out.print(mioArray[colonne][righe] + " ");
+                    sommacolonne = sommacolonne + mioArray[righe][colonne];
+                }
+                // Creo un array con dati delle somme
+                // in una stringa - // 566*586*569*568
+                // uso split
+
+                String tmpSomma = sommacolonne + "#";
+                totaliProgressivi = totaliProgressivi.concat(tmpSomma);
+                sommacolonne = 0;
+                System.out.println();
+
+
+            }
+
+        }// Fine
+        // operazione di split
+        System.out.println("----------------");
+        String[] splittati = totaliProgressivi.split("#");
+        for (String elementi: splittati) {
+            System.out.print(elementi + " ");
+
+        }
+        System.out.println();
+
+
+        // usando l'array mioarray sommo i valori  dei singoli array
+
+
+// implemento per stampa solo della matrice;
+        for (int[] righe1 : mioArray) {
+            System.out.println();
+            for (int colonne1 : righe1) {
+                System.out.print(colonne1 + "\t");
 
             }
         }
-
 
     }
 }
